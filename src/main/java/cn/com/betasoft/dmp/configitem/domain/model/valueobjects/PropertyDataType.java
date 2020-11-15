a07cefc76ada2c40ede16091738dbedcceb7d2a3
haochen21 <chenhao21@163.com>
2020-11-15 16:12:47 +0800

    beta

--- src/main/java/cn/com/betasoft/dmp/configitem/domain/model/valueobjects/PropertyDataType.java
+++ src/main/java/cn/com/betasoft/dmp/configitem/domain/model/valueobjects/PropertyDataType.java
@@ -3,7 +3,7 @@ package cn.com.betasoft.dmp.configitem.domain.model.valueobjects;
 public enum PropertyDataType {
 
     BIGINT("长整形"), BOOLEAN("布尔型"), BYTEA("字节流"), INTEGER("整形"),
-    JSONB("json"), REAL("小数"), TIMESTAMP("时间"), UUID("uuid");
+    JSONB("json"), REAL("小数"), STRING("字符窜"), TIMESTAMP("时间"), UUID("uuid");
 
     private String description;
 
--- src/main/resources/db/migration/V1.0.1__init.sql
+++ src/main/resources/db/migration/V1.0.1__init.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE config_type
 (
   id UUID DEFAULT uuid_generate_v4(),
@@ -15,7 +17,7 @@ CREATE TABLE config_type
 ALTER TABLE config_type ADD CONSTRAINT unique_config_type unique(name);
 
 CREATE TYPE property_status AS ENUM ('DRAFT', 'PUBLISHED', 'DISABLE');
-CREATE TYPE property_data_type AS ENUM ('BIGINT', 'BOOLEAN', 'BYTEA', 'INTEGER', 'JSONB', 'REAL', 'TIMESTAMP', 'UUID');
+CREATE TYPE property_data_type AS ENUM ('BIGINT', 'BOOLEAN', 'BYTEA', 'INTEGER', 'JSONB', 'REAL', 'STRING', 'TIMESTAMP', 'UUID');
 
 CREATE TABLE config_type_property
 (
@@ -26,7 +28,7 @@ CREATE TABLE config_type_property
   config_type_id UUID,
   config_type_name VARCHAR(255),
   status property_status default 'DRAFT',
-  data_type property_data_type default 'STRING'
+  data_type property_data_type default 'STRING',
   data_length smallint,
   default_value VARCHAR(255),
   meta_data JSONB DEFAULT '{}',
